f994d506ab59a991e57f36fa338c2dcf022e9490
EOosterhout <98946356+EOosterhout@users.noreply.github.com>
2023-03-03 11:05:56 +0100

    Update Retrieve Compounds from FooDB.R

--- Retrieve Compounds from FooDB.R
+++ Retrieve Compounds from FooDB.R
@@ -13,10 +13,10 @@ food <- read.csv('Food.csv')
 content <- read.csv('Content.csv')
 id <- read_xlsx('idlist.xlsx')
 
-#Filter for foods asked in FFQ
+#Filter for foods asked in FFQ and add food_id column to idlist
 ffq <- food[match(id$public_id, food$public_id), ]
 id <- cbind(id, ffq$id) 
-colnames(id)[2] <- 'food_id'
+colnames(id)[5] <- 'food_id'
 write.csv(id, file = "IDffq.csv")
 
 #Filter food item compounds
@@ -24,24 +24,35 @@ FFQ_ID <- id$food_id
 
 ffq_comp <- filter(compounds, compounds$food_id == FFQ_ID)
 ffq_comp <- as.data.frame(ffq_comp)
+ffq_comp <- subset(ffq_comp, select = -c(orig_food_id))
 
 ffq_cont <- filter(content, content$food_id == FFQ_ID)
+ffq_cont <- subset(ffq_cont, select = -c(orig_food_id))
 
-#Remove NA values from dataframe (optional)
-x <- complete.cases(ffq_comp$orig_content)
-ffq_comp <- filter(ffq_comp, x)
+#Adding data from compound and content file together
+full_chem <- ffq_comp %>% full_join(ffq_cont)
+full_chem <- subset(full_chem, select = -c(id, source_id, orig_citation, creator_id, updater_id, created_at, updated_at))
+write.table(full_chem, file = 'full_chem.txt')
 
-y <- complete.cases(ffq_cont$orig_content)
-ffq_cont <- filter(ffq_cont, y)
+#Add ffq_group to food_id
+write.table(full_chem$food_id, file = 'ffqfood.txt')
 
-#Splitting dataframe into the different fooditems
-splitcomp <- split.data.frame(ffq_comp, ffq_comp$food_id)
-splitcont <- split.data.frame(ffq_cont, ffq_cont$food_id)
+foodffq <- read_xlsx('ffqfood.xlsx')
 
-#Saving each element of listoutput as .csv file
-sapply(names(splitcomp), 
-       function (x) write.table(splitcomp[[x]], file=paste(x, "txt", sep=".") ) 
+full_chem <- full_chem[order(full_chem$food_id), ] #descending order
+chem_ffq <- cbind(full_chem, foodffq)
+
+
+# Splitting dataframe into the different ffq elements 
+# expect 62 elements minus two elements not found in compound/content datafiles --> 60
+splitchem_1 <- split.data.frame(chem_ffq, chem_ffq$ffq_group_1)
+splitchem_2 <- split.data.frame(chem_ffq, chem_ffq$ffq_group_2)
+splitchem_3 <- split.data.frame(chem_ffq, chem_ffq$ffq_group_3)
+
+splitchem_ffq <- c(splitchem_1, splitchem_2, splitchem_3)
+splitchem_ffq[["NA"]] <- NULL
+
+#Saving each element of list output as .txt file
+sapply(names(splitchem_ffq), 
+       function (x) write.table(splitchem_ffq[[x]], file=paste(x, "txt", sep=".") ) 
        )
-sapply(names(splitcont), 
-       function (x) write.table(splitcont[[x]], file=paste(x, "txt", sep=".") ) 
-       )  
