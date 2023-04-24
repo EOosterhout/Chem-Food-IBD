b4c31d6b56c8d6a13b1e51c1c8af1afaeaaea724
Emily <98946356+EOosterhout@users.noreply.github.com>
2023-04-24 14:14:41 +0200

    Update Automized diet to chem.R

--- Automized diet to chem.R
+++ Automized diet to chem.R
@@ -20,6 +20,7 @@ library(writexl)
 # Load data
 chem_diet <- read_xlsx('chem_diet_combined_V2.xlsx')
 chem <- read_xlsx('transposed_ffqchem_V2.xlsx')
+diet_raw <- read_xlsx('diet_raw_v2.xlsx')
 
 ##======================================= CONVERSION FFQ_GROUP/DAY TO CHEM/DAY =======================================##
 chemdata <- as.character(colnames(chem_diet[,2:1109]))
@@ -61,6 +62,7 @@ multiply_columns_list <- function(df_list, participant_id) {
 
 # call the function to multiply each dataframe's columns with the specified column in list
 df_list <- multiply_columns_list(chem_diet_p, participant_id)
+df_list_2 <- multiply_columns_list(chem_diet_p, participant_id)
 
 # Add ffq groups to each list item
 chem_diet_m <- list()
@@ -70,3 +72,23 @@ for (t in participant_id){
   chem_diet_m[[t]] <- output
 }
 
+##============================= SUM OF DIETARY COMPOUND CONSUMED PER PARTICIPANT ==============##
+
+# ColSums of each dietary component consumed per participant
+chem_diet_s <- list()
+for (t in participant_id){
+  out <- as.data.frame(colSums(df_list_2[[t]], na.rm = TRUE))
+  colnames(out) <- t
+  chem_diet_s[[t]] <- out
+}
+
+# New df with sum of dietary component consumed per participant
+sum_chem_diet <- bind_cols(chem_diet_s) # put all items of the list into one df
+sum_chem_diet_t <- as.data.frame(t(sum_chem_diet)) #transform df to get compounds as colnames
+sum_chem_diet_c <- setDT(sum_chem_diet_t, keep.rownames = 'UMCGIBDResearchIDorLLDeepID')[] #put rownames in first column
+
+# Filter participants containing dietary data from diet_raw (1985/2116)
+diet_clean <- diet_raw[1:1985, 1:6]
+
+# Bind dietary info to total component data
+chem_diet_merge <- merge(diet_clean, sum_chem_diet_c, on = 'UMCGIBDResearchIDorLLDeepID')
